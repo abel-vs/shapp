15e4e8e3495aeef03e03626fca4d4441fe4849aa
Abel-VS <you@example.com>
2020-09-12 10:52:22 +0200

    Added searchbar
    Stole it from: https://stackoverflow.com/questions/58908968/how-to-implement-a-flutter-search-app-bar

--- lib/screens/search_page.dart
+++ lib/screens/search_page.dart
@@ -1,11 +1,95 @@
 import 'package:flutter/material.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  TextEditingController _searchQueryController = TextEditingController();
+  bool _isSearching = true;
+  String searchQuery = "Search Query";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Search"),),
+      appBar: AppBar(
+        leading: _isSearching
+            ? const BackButton()
+            : Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: CircularProgressIndicator(
+                  strokeWidth: 2,
+                ),
+              ),
+        title: _isSearching ? _buildSearchField() : Text("Wut"),
+        actions: _buildActions(),
+      ),
       body: Center(child: Text("Search dat shit boii")),
     );
+  }
+
+  Widget _buildSearchField() {
+    return TextField(
+      controller: _searchQueryController,
+      autofocus: true,
+      decoration: InputDecoration(
+        hintText: "Zoek een product",
+        border: InputBorder.none,
+        hintStyle: TextStyle(color: Colors.white30),
+      ),
+      style: TextStyle(color: Colors.white, fontSize: 16.0),
+      onChanged: (query) => updateSearchQuery(query),
+    );
+  }
+
+  List<Widget> _buildActions() {
+    if (_isSearching) {
+      return <Widget>[
+        (_searchQueryController == null || _searchQueryController.text.isEmpty)
+            ? IconButton(icon: Icon(Icons.camera_alt), onPressed: () => print("Open QR scan"),)
+            : IconButton(
+                icon: const Icon(Icons.clear),
+                onPressed: () =>  _clearSearchQuery(),
+              ),
+      ];
+    }
+
+    return <Widget>[
+      IconButton(
+        icon: const Icon(Icons.search),
+        onPressed: _startSearch,
+      ),
+    ];
+  }
+
+  void _startSearch() {
+    ModalRoute.of(context)
+        .addLocalHistoryEntry(LocalHistoryEntry(onRemove: _stopSearching));
+
+    setState(() {
+      _isSearching = true;
+    });
+  }
+
+  void updateSearchQuery(String newQuery) {
+    setState(() {
+      searchQuery = newQuery;
+    });
+  }
+
+  void _stopSearching() {
+    _clearSearchQuery();
+
+    setState(() {
+      _isSearching = false;
+    });
+  }
+
+  void _clearSearchQuery() {
+    setState(() {
+      _searchQueryController.clear();
+      updateSearchQuery("");
+    });
   }
 }
