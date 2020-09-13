751928831c9d2befa4a9a332fa9d6c1d74a67a0e
Abel Van Steenweghen <a.vansteenweghen@student.tudelft.nl>
2020-09-13 14:26:57 +0200

    Added listview to MorePage and added link to IntroTutorial

--- lib/screens/more_page.dart
+++ lib/screens/more_page.dart
@@ -1,11 +1,29 @@
+import 'package:Shapp/screens/intro_page.dart';
 import 'package:flutter/material.dart';
 
 class MorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Meer"),),
-      body: Center(child: Text("Meer")),
+      appBar: AppBar(
+        title: Text("Meer"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Icon(
+              Icons.help,
+              color: Colors.black,
+            ),
+            title: Text("Hoe te gebruiken"),
+            onTap: () => Navigator.of(context, rootNavigator: true).push(
+              MaterialPageRoute(
+                builder: (context) => IntroPage(),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
