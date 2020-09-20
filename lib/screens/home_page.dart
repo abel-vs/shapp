47e94f2b9254e6944ca4831d022ce9de03adb59e
Abel Van Steenweghen <a.vansteenweghen@student.tudelft.nl>
2020-09-20 12:35:10 +0200

    Filled home screen with possible views

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,6 +2,8 @@ import 'package:Shapp/screens/map_page.dart';
 import 'package:Shapp/screens/search_page.dart';
 import 'package:Shapp/services/app_localizations.dart';
 import 'package:Shapp/widgets/search_bar.dart';
+import 'package:Shapp/widgets/shop_card.dart';
+import 'package:Shapp/widgets/sliver_title.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,17 +17,57 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Shapp"),
-        actions: [IconButton(icon: Icon(Icons.shopping_basket_outlined), onPressed: () => print("Yeet"), )],
-      ),
-      body: Stack(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: SearchBar(),
-          ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_basket_outlined),
+            onPressed: () => print("Yeet"),
+          )
         ],
       ),
-//      backgroundColor:,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: SearchBar(),
+            centerTitle: true,
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            floating: true,
+            pinned: true,
+            snap: false,
+            toolbarHeight: 100,
+          ),
+          SliverTitle(title: "Koop iemand een cadeautje"),
+          _buildScrollView(),
+          SliverTitle(title: "In de kijker"),
+          _buildScrollView(),
+          SliverTitle(title: "Voor jou"),
+          _buildScrollView(),
+          SliverTitle(title: "Populaire producten"),
+          _buildScrollView(),
+          SliverTitle(title: "Populaire winkels"),
+          _buildScrollView(),
+        ],
+      ),
     );
   }
+
+  SliverToBoxAdapter _buildScrollView() {
+    return SliverToBoxAdapter(
+            child: Container(
+              height: 150.0,
+              child: ListView(
+                padding: EdgeInsets.all(8),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Card(child: Placeholder(fallbackWidth: 300,)),
+                  Card(child: Placeholder(fallbackWidth: 300,)),
+                  Card(child: Placeholder(fallbackWidth: 300,)),
+                ],
+              ),
+            ),
+          );
+  }
 }
+
+
