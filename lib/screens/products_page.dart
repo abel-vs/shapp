cc75bdfd578c16788e017a99e49d9c012b79debc
Abel Van Steenweghen <a.vansteenweghen@student.tudelft.nl>
2020-09-20 12:06:58 +0200

    Added basic sliver structure

--- lib/screens/products_page.dart
+++ lib/screens/products_page.dart
@@ -1,3 +1,5 @@
+import 'package:Shapp/widgets/search_bar.dart';
+import 'package:Shapp/widgets/shop_tile.dart';
 import 'package:flutter/material.dart';
 
 class ProductsPage extends StatelessWidget {
@@ -5,7 +7,50 @@ class ProductsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Producten")),
-      body: Center(child: Text("Producten")),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              title: SearchBar(),
+              centerTitle: true,
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.transparent,
+              shadowColor: Colors.transparent,
+              floating: true,
+              pinned: false,
+              snap: false,
+              toolbarHeight: 100,
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  "Categorieën",
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ),
+            ),
+            SliverGrid.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+              children: [
+                Card(),
+                Card(),
+                Card(),
+                Card(),
+                Card(),
+                Card(),
+                Card(),
+                Card(),
+                Card(),
+                Card(),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
