17229b8c89906291a48a9c1031c505d7be21028c
Abel Van Steenweghen <a.vansteenweghen@student.tudelft.nl>
2020-10-09 10:25:11 +0200

    Added categoryProducts method

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,29 +1,43 @@
 import 'dart:developer';
 
+import 'package:Shapp/models/category.dart';
 import 'package:Shapp/models/product.dart';
 import 'package:Shapp/services/firebase_path.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'firestore_service.dart';
 
 abstract class Database {
   Stream<List<Product>> productsStream();
+
+  Stream<List<Product>> productCategoryStream(Category category);
 }
 
 class FirestoreDatabase implements Database {
   final _service = FirestoreService.instance;
 
+  Function productBuilder = (data, documentID) {
+    return Product(
+        id: documentID,
+        name: data['name'],
+        image: data['image'],
+        category: data['category'],
+        price: data['price'],
+        info: data['info']);
+  };
+
   @override
   Stream<List<Product>> productsStream() {
     return _service.collectionStream(
+        path: FirebasePath.products(), builder: productBuilder);
+  }
+
+  @override
+  Stream<List<Product>> productCategoryStream(Category category) {
+    return _service.collectionStream(
         path: FirebasePath.products(),
-        builder: (data, documentID) {
-          return Product(
-              id: documentID,
-              name: data['name'],
-              image: data['image'],
-              category: data['category'],
-              price: 69,
-              info: data['info']);
-        });
+        builder: productBuilder,
+        queryBuilder: (query) =>
+            query.where('category', isEqualTo: category.name));
   }
 }
