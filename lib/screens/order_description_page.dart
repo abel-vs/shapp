e411505f265e18886cb7b55422340b95234c56f3
Abel Van Steenweghen <a.vansteenweghen@student.tudelft.nl>
2020-11-22 13:57:31 +0100

    Added Picture Picker

--- lib/screens/order_description_page.dart
+++ lib/screens/order_description_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:shapp/widgets/expanded_button.dart';
 import 'package:shapp/widgets/order_title_block.dart';
 
@@ -13,6 +16,9 @@ class OrderDescriptionPage extends StatefulWidget {
 }
 
 class _OrderDescriptionPageState extends State<OrderDescriptionPage> {
+  File _image;
+  final picker = ImagePicker();
+
   @override
   void initState() {
     widget.descriptionController.addListener(() {
@@ -56,19 +62,108 @@ class _OrderDescriptionPageState extends State<OrderDescriptionPage> {
     return Expanded(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: TextField(
-          textAlignVertical: TextAlignVertical.top,
-          controller: widget.descriptionController,
-          decoration: InputDecoration(
-            labelText: "Beschrijving",
-            alignLabelWithHint: true,
-            contentPadding: EdgeInsets.all(20.0),
-            border: OutlineInputBorder(),
-          ),
-          keyboardType: TextInputType.multiline,
-          expands: true,
-          maxLines: null,
+        child: Stack(
+          children: [
+            TextField(
+              textAlignVertical: TextAlignVertical.top,
+              controller: widget.descriptionController,
+              decoration: InputDecoration(
+                labelText: "Beschrijving",
+                alignLabelWithHint: true,
+                contentPadding: EdgeInsets.all(20.0),
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.multiline,
+              expands: true,
+              maxLines: null,
+            ),
+            Positioned(
+              right: 20,
+              bottom: 20,
+              child: _image == null
+                  ? FloatingActionButton(
+                      child: Icon(
+                        Icons.add_photo_alternate_outlined,
+                        color: Theme.of(context).canvasColor,
+                      ),
+                      onPressed: getImage,
+                    )
+                  : InkWell(
+                      onTap: () => deleteImage(),
+                      borderRadius: BorderRadius.circular(300.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(300.0),
+                        child: Image.file(
+                          _image,
+                          fit: BoxFit.cover,
+                          height: 50,
+                          width: 50,
+                        ),
+                      ),
+                    ),
+            )
+          ],
         ),
+      ),
+    );
+  }
+
+  Future getImage() async {
+    PickedFile pickedFile;
+
+    await showDialog(
+      context: context,
+      child: Dialog(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              title: Text("Make a photo"),
+              leading: Icon(Icons.image),
+              onTap: () async {
+                pickedFile = await picker.getImage(source: ImageSource.camera);
+                Navigator.of(context).pop();
+              },
+            ),
+            Divider(height: 0),
+            ListTile(
+              title: Text("Pick from gallery"),
+              leading: Icon(Icons.camera_alt),
+              onTap: () async {
+                pickedFile = await picker.getImage(source: ImageSource.gallery);
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+
+    setState(() {
+      _image = File(pickedFile.path);
+    });
+  }
+
+  deleteImage() {
+    showDialog(
+      context: context,
+      child: AlertDialog(
+        title: Text("Delete the image?"),
+        actions: [
+          FlatButton(
+            child: Text("No"),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          FlatButton(
+            child: Text("Yes"),
+            onPressed: () {
+              setState(() {
+                _image = null;
+              });
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
     );
   }
