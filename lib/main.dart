101495020c14280a15dd1f1567b3495a03bb8258
Abel Van Steenweghen <a.vansteenweghen@student.tudelft.nl>
2021-01-06 14:55:19 +0100

    Made Stripe key based on build mode
    Now it doesn't have to be changed each time

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 import 'package:shapp/navigation/landing_page.dart';
 import 'package:shapp/pages/error_page.dart';
 import 'package:shapp/pages/policy_page.dart';
@@ -7,28 +13,22 @@ import 'package:shapp/services/auth.dart';
 import 'package:shapp/services/database.dart';
 import 'package:shapp/services/preferences_provider.dart';
 import 'package:shapp/themes.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:provider/provider.dart';
-import 'services/app_localizations.dart';
-
 import 'package:stripe_payment/stripe_payment.dart';
+
+import 'services/app_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   StripePayment.setOptions(StripeOptions(
-      publishableKey:
-          // "pk_live_51HoqjPCn8fuy3fijbmvcV2VTTDOH87JjHj1coI6n9FsmVRGueXQkgdQgMvMRxvIUDQqwPAfjZL8j2un6e51w8v4P00YpVNZpIw",
-          "pk_test_51HoqjPCn8fuy3fijvWHsIaEjvVhPBvIkB4hTnavS9gSMFcFxS9HCXsLCvpMb9uaVMD9mAFCOIbfZXVVDKNSnvipI00WnlnLe5X",
+      publishableKey: kReleaseMode
+          ? "pk_live_51HoqjPCn8fuy3fijbmvcV2VTTDOH87JjHj1coI6n9FsmVRGueXQkgdQgMvMRxvIUDQqwPAfjZL8j2un6e51w8v4P00YpVNZpIw"
+          : "pk_test_51HoqjPCn8fuy3fijvWHsIaEjvVhPBvIkB4hTnavS9gSMFcFxS9HCXsLCvpMb9uaVMD9mAFCOIbfZXVVDKNSnvipI00WnlnLe5X",
       //YOUR_PUBLISHABLE_KEY
       merchantId: "Shapp", //YOUR_MERCHANT_ID
       androidPayMode: 'test'));
   // Set custom error screen
-  ErrorWidget.builder = (FlutterErrorDetails flutterErrorDetails) =>
-      ErrorPage(flutterErrorDetails.exception);
+  ErrorWidget.builder = (FlutterErrorDetails flutterErrorDetails) => ErrorPage(flutterErrorDetails.exception);
   runApp(MyApp());
 }
 
@@ -39,16 +39,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations(
-        [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
 
     return MultiProvider(
       providers: [
         Provider<AuthBase>(create: (context) => Auth()),
         Provider<Database>(create: (context) => FirestoreDatabase()),
         Provider<FirebaseAnalytics>(create: (context) => analytics),
-        ChangeNotifierProvider<PreferencesProvider>(
-            create: (context) => PreferencesProvider()),
+        ChangeNotifierProvider<PreferencesProvider>(create: (context) => PreferencesProvider()),
       ],
       child: Builder(
         builder: (BuildContext context) => Consumer<PreferencesProvider>(
@@ -75,14 +73,11 @@ class MyApp extends StatelessWidget {
               routes: {
                 // When navigating to the "/" route, build the FirstScreen widget.
                 'privacy_policy': (context) => PolicyPage(
-                    title: "privacy_policy",
-                    content: AppLocalizations.of(context)
-                        .translate("privacy_policy_link")),
+                    title: "privacy_policy", content: AppLocalizations.of(context).translate("privacy_policy_link")),
                 // When navigating to the "/second" route, build the SecondScreen widget.
                 'general_conditions': (context) => PolicyPage(
                     title: "general_conditions",
-                    content: AppLocalizations.of(context)
-                        .translate("general_conditions_link")),
+                    content: AppLocalizations.of(context).translate("general_conditions_link")),
               },
 
               /// Localization
@@ -95,8 +90,7 @@ class MyApp extends StatelessWidget {
               locale: preferences.locale,
               supportedLocales: supportedLocales.values,
               // Returns a locale which will be used by the app
-              localeResolutionCallback: (locale, supportedLocales) =>
-                  localeResolution(locale, supportedLocales),
+              localeResolutionCallback: (locale, supportedLocales) => localeResolution(locale, supportedLocales),
             );
           },
         ),
